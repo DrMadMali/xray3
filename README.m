4102b5cd8fa48f935c3f81818b683424c6975f6c
DrMadMali <127998313+DrMadMali@users.noreply.github.com>
2023-09-01 01:31:16 +0100

    Create README.m

---
+++ README.m
@@ -0,0 +1,14 @@
+# X-Ray Image Dataset
+
+This repository contains an x-ray image dataset for fracture detection.
+
+## Dataset Details
+
+- **Fracture Images:** [Link to Fracture Images](https://drive.google.com/drive/folders/1jzHQfauFoia1qdqYMUtjjVxmrKnWPqgF)
+- **Normal Images:** [Link to Normal Images](https://drive.google.com/drive/folders/1iGV1v3P_VCWtSyu90ktm-eodhZcjfB7t)
+
+## Usage
+
+You can use the images in this dataset for training and evaluating your fracture detection models.
+
+Please remember to follow ethical guidelines and data protection regulations when using this dataset.
